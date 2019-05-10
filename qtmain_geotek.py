72e30d186b00a3f70c99301bd947e8370321c98a
Brian G <sorghumking@yahoo.com>
2019-05-09 19:35:09 -0500

    Simple validation of inputs before conversion; retain selected ruler when default settings are saved; init app path and rulers before init'ing prefs

--- qtmain_geotek.py
+++ qtmain_geotek.py
@@ -17,11 +17,12 @@ class MainWindow(QtWidgets.QDialog):
         QtWidgets.QDialog.__init__(self)
         self.VERSION = "2.2"
         self.app = app
-        self.app_path = None # init'd in self.initPrefs()
+        self.app_path = None # init'd in self.initAppPath()
 
+        self.initAppPath()
         self.initGUI()
-        self.initPrefs()
         self.installRulers()
+        self.initPrefs()
 
     def initGUI(self):
         self.setWindowTitle("LacCore/CSDCO Geotek Image Converter v{}".format(self.VERSION))
@@ -45,7 +46,7 @@ class MainWindow(QtWidgets.QDialog):
         dpiLayout.addWidget(self.trim)
         dpiLayout.addWidget(QtWidgets.QLabel("inches from top of core image"))
         dpiLayout.addSpacing(20)
-        dpiLayout.addWidget(QtWidgets.QLabel("Scale ICD-ready image by:"))
+        dpiLayout.addWidget(QtWidgets.QLabel("Resize ICD-ready image by:"))
         dpiLayout.addWidget(self.icdScaling)
         dpiLayout.addWidget(QtWidgets.QLabel("%"))
 
@@ -86,12 +87,14 @@ class MainWindow(QtWidgets.QDialog):
     def showProgressLayout(self, show):
         self.stackedLayout.setCurrentIndex(1 if show else 0)
 
-    def initPrefs(self):
+    def initAppPath(self):
         try:
             self.app_path = common.get_app_path()
         except common.InvalidApplicationPathError as iape:
             errbox(self, "Invalid Application Path", "Couldn't find application directory, exiting.")
             raise iape # re-raise and bail
+
+    def initPrefs(self):
         prefPath = os.path.join(self.app_path, "prefs.pk")
         self.prefs = Preferences(prefPath)
         self.installPrefs()
@@ -118,6 +121,9 @@ class MainWindow(QtWidgets.QDialog):
         self.dpi.setText(self.prefs.get("dpi", "508"))
         self.trim.setText(self.prefs.get("trim", "0.25"))
         self.icdScaling.setText(self.prefs.get("icdScaling", "30"))
+        ruler = self.prefs.get("ruler", "")
+        rulerIdx = self.rulerCombo.findText(ruler)
+        self.rulerCombo.setCurrentIndex(rulerIdx if rulerIdx >= 0 else 0)
         # default to input file name
         self.outputNamingCombo.setCurrentIndex(self.prefs.get("outputNaming", 0))
 
@@ -129,6 +135,7 @@ class MainWindow(QtWidgets.QDialog):
         self.prefs.set("dpi", self.dpi.text())
         self.prefs.set("trim", self.trim.text())
         self.prefs.set("icdScaling", self.icdScaling.text())
+        self.prefs.set("ruler", self.rulerCombo.currentText())
         self.prefs.set("outputNaming", self.outputNamingCombo.currentIndex())
 
     # override QWidget.closeEvent()
@@ -151,8 +158,18 @@ class MainWindow(QtWidgets.QDialog):
         geotek.setProgressListener(self.progressPanel)
         try:
             dpi = float(self.dpi.text())
-            trim = float(self.trim.text())
+            if dpi <= 0:
+                errbox(self, "Invalid DPI", "DPI must be greater than zero.")
+                return
+            trimTxt = self.trim.text()
+            trim = float(trimTxt if len(trimTxt) > 0 else 0)
+            if trim < 0:
+                errbox(self, "Invalid trim length", "Trim length cannot be negative.")
+                return
             icdScaling = float(self.icdScaling.text())
+            if icdScaling <= 0:
+                errbox(self, "Invalid ICD Scaling", "ICD scaling % must be greater than zero.")
+                return
             parentDirBasename = self.outputNamingCombo.currentIndex() == 1
             for imgPath in imgFiles:
                 if parentDirBasename:
@@ -165,6 +182,8 @@ class MainWindow(QtWidgets.QDialog):
             errbox(self, "Ruler Too Short", "{}".format(e.message))
         except common.UnexpectedColorDepthError as e:
             errbox(self, "Unexpected Color Depth", "{}".format(e.message))
+        except ValueError: # raised by float()
+            errbox(self, "Expected Numeric Input", "Invalid DPI, trim, or ICD Scaling value, all of which must be numeric.")
         except:
             err = sys.exc_info()
             errbox(self, "Process failed", "{}".format("Unhandled error {}: {}".format(err[0], err[1])))
